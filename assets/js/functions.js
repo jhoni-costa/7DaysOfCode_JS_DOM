d9ecc007d840932a7aa3981e5178299fd2899b1d
jhoni-costa <jhoni_costa@outlook.com>
2022-05-16 08:52:50 -0300

    #7DaysOfCode - JS e DOM 3/7 - Salvando no localStorage

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,19 @@
 dataForm = () =>{
+
+    
+    let pessoa = {};
     const name = document.getElementById('name').value;
     const birthday = document.getElementById('date').value;
     
+
     let arrBirthday = birthday.split('-');
-    // console.log(arrBirthday.reverse());
     arrBirthday = arrBirthday.reverse().join('/');
     console.log(`Nome: ${name} \nData de Nascimento: ${arrBirthday}`);
+    
+    pessoa = {
+        name: name,
+        birthday: arrBirthday
+    }
+    localStorage.setItem('pessoas',JSON.stringify(pessoa));    
     
 }
